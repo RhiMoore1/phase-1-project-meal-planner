fc67e4ca28ecdd6ece8e0de67273003b78e55015
RhiMoore1 <126516175+RhiMoore1@users.noreply.github.com>
2023-04-29 12:34:30 -0700

    updates main.js fetch with a forEach

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ console.log('main.js is working')
 
 fetch("http://localhost:3000/FoodChoices")
 .then(res => res.json())
-.then(json => console.log(json))
+.then(foodChoices => {
+    const proteinChoicesDiv = document.querySelector("#proteinChoicesDiv");
+    foodChoices.forEach(foodChoice => {
+        if (foodChoice.type === "PROTEIN") {
+            let proteinChoice1 = document.createElement('p');
+            proteinChoice1.textContent = foodChoice.title;
+            proteinChoicesDiv.appendChild(proteinChoice1);
+        }
+    })
+})
+
